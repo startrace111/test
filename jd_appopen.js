4f003ca2a087a94f7cf4000d11929579c8b2ae1e
朱安杰 <zhuanjie@zhuanjiedeMac-mini.local>
2023-12-01 11:00:52 +0800

    jd

---
+++ jd_appopen.js
@@ -0,0 +1,35 @@
+const $ = new Env("app_open")
+
+if ($request.headers) {
+    let cookie = ($request.headers.Cookie || $request.headers['Cookie'] || $request.headers['cookie'] || '')
+    let pt_key = cookie.match(/(pt_key=[^;]*)/)[1]
+    let pt_pin = cookie.match(/(pt_pin=[^;]*)/)[1]
+    if (pt_key && pt_pin) {
+        console.log('================')
+        console.log(`${pt_key};${pt_pin};`)
+        console.log('================')
+
+        const options = {
+            "url": `换成你的域名/cookie/update`,
+            'body': JSON.stringify({
+                "pt_key": pt_key,
+                "pt_pin": pt_pin
+            })
+        }
+        $.post(options, (err, resp, data) => {
+            try {
+                if (err) {
+                    $.msg("app_open获取成功！", `更新cookie失败: ${JSON.stringify(err)}`)
+                    console.log(`pp_open获取成功,更新cookie失败: ${JSON.stringify(err)}`)
+                } else {
+                    console.log(`更新cookie: ${data}`)
+                }
+            } catch (e) {
+                $.logErr(e, resp)
+            }
+        })
+    }
+}
+
+$.done($request.headers)
+
